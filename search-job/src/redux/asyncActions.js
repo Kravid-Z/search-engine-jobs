eca4e6815ac201a054f41793856615f9405d040d
“Kravid-Z” <kravidz64@gmail.com>
2021-05-20 16:56:54 +0200

    problem solved now its fetching and storing in redux store

--- search-job/src/redux/asyncActions.js
+++ search-job/src/redux/asyncActions.js
@@ -6,9 +6,10 @@ import {
 
 export const fetchUserSearch =  (location, positition) => {
   return async (dispatch, getState) =>{try {
-    // dispatch({ type: FETCH_USER_SEARCH });
+    dispatch({ type: FETCH_USER_SEARCH });
+    console.log(location, positition)
     let resp = await fetch(
-      `https://striveschool-api.herokuapp.com/api/jobs?description=${location}&location=${positition}`
+      `https://striveschool-api.herokuapp.com/api/jobs?description=${positition}&location=${location}`
     );
     let results = await resp.json();
     console.log(results)
